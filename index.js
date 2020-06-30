c3b4b6abb0510772d22d64ed284c7d60bfbd51f2
chg1213 <chg1213@naver.com>
2020-06-30 22:47:21 +0900

    Handling Routes with Express &  ES6 using Babel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+import express from "express"
 const app = express()
 
 const PORT = 4000;
@@ -7,4 +7,15 @@ const handleListening = () => {
     console.log(`Listening to http://localhost:${PORT}`)
 }
 
+const handleHome = (req, res) => {
+    console.log(req)
+    res.send("Hello from home")
+}
+
+const handleProfile = (req, res) => {
+    res.send("You are on my profile")
+}
+
+app.get("/", handleHome)
+app.get("/profile", handleProfile)
 app.listen(PORT, handleListening)
