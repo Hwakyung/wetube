70764737134544958b5914c3d27d171f1d2c1e2e
chg1213 <chg1213@naver.com>
2020-07-04 16:02:53 +0900

    search controller

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,10 @@
 //render함수의 첫번째 인자는 템플릿이고 두 번째 인자는 정보가 담긴 객체
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+export const search = (req, res) => {
+    const { query: { term: searchingBy } } = req; // const searchingBy = req.query.term;과 동일
+    res.render("search", { pageTitle: "Search", searchingBy })
+
+};
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
