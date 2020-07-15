ffb6893e250bba3a55e764d0795173c88e7cc358
chg1213 <chg1213@naver.com>
2020-07-15 21:10:05 +0900

    More Controlers

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,5 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 }
@@ -23,7 +25,12 @@ export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" }
 export const postLogin = (req, res) => {
     res.redirect(routes.home)
 }
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+
+export const logout = (req, res) => {
+    //to do : process logout
+    res.redirect(routes.home)
+}
+
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db"
+import routes from "../routes"
 
 //render함수의 첫번째 인자는 템플릿이고 두 번째 인자는 정보가 담긴 객체
 export const home = (req, res) => {
@@ -9,7 +10,20 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy, videos })
 
 };
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+    const {
+        body: {
+            file,
+            title,
+            description
+        }
+
+    } = req
+    // to do : upload and save video
+    res.redirect(routes.videoDetail(234346))
+}
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
