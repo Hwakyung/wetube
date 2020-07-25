8380c599975fc99b01ee245e7b2f91e4d2ca3d8e
chg1213 <chg1213@naver.com>
2020-07-25 13:04:09 +0900

    Searching Videos

--- controller/videoController.js
+++ controller/videoController.js
@@ -11,12 +11,22 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
-export const search = (req, res) => {
+
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req; // const searchingBy = req.query.term;과 동일
-  res.render("search", { pageTitle: "Search", searchingBy });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
+
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 export const postUpload = async (req, res) => {
